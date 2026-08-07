9aecbb56e5c8d689168622adac2786f160207244
xcasillas-lab <xcasillas@gmail.com>
2026-08-07 14:34:13 -0500

    Delete js directory

--- js/rules.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelectorAll('.rules-jumpbar [data-rule-target]').forEach(function(btn){
-  btn.addEventListener('click', function(){
-    var target = document.getElementById(btn.getAttribute('data-rule-target'));
-    if(target){ target.scrollIntoView({behavior:'smooth', block:'start'}); }
-  });
-});
